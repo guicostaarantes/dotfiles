916d487e9b3b48cc85a6c1efa3527f79b8661c53
Guilherme Arantes <arantes.g@outlook.com>
2025-10-24 18:15:53 -0300

    feat: gituser as copy rather than symlink

--- dotfiles-symlinks.sh
+++ dotfiles-symlinks.sh
@@ -1,19 +1,21 @@
 SCRIPTPATH=$(readlink -f "$0")
 SCRIPTDIR=$(dirname "$SCRIPTPATH")
 
-./make-symlink.sh $SCRIPTDIR/fontconfig ~/.config/fontconfig
-./make-symlink.sh $SCRIPTDIR/fuzzel ~/.config/fuzzel
-./make-symlink.sh $SCRIPTDIR/git/gitconfig ~/.gitconfig
-./make-symlink.sh $SCRIPTDIR/kitty ~/.config/kitty
-./make-symlink.sh $SCRIPTDIR/mako ~/.config/mako
-./make-symlink.sh $SCRIPTDIR/mime/mimeapps.list ~/.config/mimeapps.list
-./make-symlink.sh $SCRIPTDIR/niri ~/.config/niri
-./make-symlink.sh $SCRIPTDIR/nvim ~/.config/nvim
-./make-symlink.sh $SCRIPTDIR/waybar ~/.config/waybar
-./make-symlink.sh $SCRIPTDIR/xcompose/ptbr ~/.XCompose
-./make-symlink.sh $SCRIPTDIR/zsh/zshrc ~/.zshrc
+./make-symlink.sh $SCRIPTDIR/fontconfig $HOME/.config/fontconfig
+./make-symlink.sh $SCRIPTDIR/fuzzel $HOME/.config/fuzzel
+./make-symlink.sh $SCRIPTDIR/git/gitconfig $HOME/.gitconfig
+./make-symlink.sh $SCRIPTDIR/kitty $HOME/.config/kitty
+./make-symlink.sh $SCRIPTDIR/mako $HOME/.config/mako
+./make-symlink.sh $SCRIPTDIR/mime/mimeapps.list $HOME/.config/mimeapps.list
+./make-symlink.sh $SCRIPTDIR/niri $HOME/.config/niri
+./make-symlink.sh $SCRIPTDIR/nvim $HOME/.config/nvim
+./make-symlink.sh $SCRIPTDIR/waybar $HOME/.config/waybar
+./make-symlink.sh $SCRIPTDIR/xcompose/ptbr $HOME/.XCompose
+./make-symlink.sh $SCRIPTDIR/zsh/zshrc $HOME/.zshrc
 
 sudo ./make-symlink.sh $SCRIPTDIR/udev/rules.d /etc/udev/rules.d
 
 # This contains personal info which should not be tied to the repo
-cp -u $SCRIPTDIR/git/gituser ~/.gituser
+if [ ! -f $HOME/.gituser ]; then
+	cp $SCRIPTDIR/git/gituser $HOME/.gituser
+fi
